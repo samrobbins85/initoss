b00b8b90fbefd04a436c2bf35871fde5db80146c
Sam Robbins <samrobbinsgb@gmail.com>
2020-09-14 15:06:56 +0100

    Change path for sitemap

--- sitemap_generator.js
+++ sitemap_generator.js
@@ -2,7 +2,7 @@ const sitemap = require("nextjs-sitemap-generator");
 
 sitemap({
 	baseUrl: "https://csnotes.me",
-	pagesDirectory: __dirname + "/notes",
+	pagesDirectory: __dirname + "/page_content",
 	targetDirectory: "public/",
 	ignoredExtensions: ["js", "map"],
 	ignoredPaths: ["[fallback]"],
