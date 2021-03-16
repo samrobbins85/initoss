630a2aaecbb5981aa4012485b9958d24053ec640
Sam Robbins <samrobbinsgb@gmail.com>
2021-03-16 15:11:49 +0000

    Remove tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,8 @@
 import "../styles/index.css";
 import "../styles/prism-atom-dark.css";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import * as gtag from "../lib/gtag";
+import {  useState } from "react";
 function MyApp({ Component, pageProps }) {
 	const [sidebar, setSidebar] = useState(false);
-	const router = useRouter();
-	useEffect(() => {
-		const handleRouteChange = (url) => {
-			gtag.pageview(url);
-		};
-		router.events.on("routeChangeComplete", handleRouteChange);
-		return () => {
-			router.events.off("routeChangeComplete", handleRouteChange);
-		};
-	}, [router.events]);
 	return (
 		<Component sidebar={sidebar} setSidebar={setSidebar} {...pageProps} />
 	);
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { GA_TRACKING_ID } from "../lib/gtag";
 class MyDocument extends Document {
 	render() {
 		return (
@@ -10,22 +9,6 @@ class MyDocument extends Document {
 						rel="shortcut icon"
 						href="/favicon.ico"
 						type="image/x-icon"
-					/>
-					<script
-						async
-						src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-					/>
-					<script
-						dangerouslySetInnerHTML={{
-							__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-						}}
 					/>
 					<script
 						type="text/javascript"
